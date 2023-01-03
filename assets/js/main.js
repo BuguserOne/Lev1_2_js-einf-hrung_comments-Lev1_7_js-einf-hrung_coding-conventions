daa070a3baffca588adbe1240f8ffe30bed8044f
BuguserOne <jinlingone@gmail.com>
2023-01-03 13:50:37 +0100

    Lev1_2_js-einführung_comments/Lev1_7_js-einführung_coding-conventions

---
+++ assets/js/main.js
@@ -0,0 +1,35 @@
+
+// This is a single line JavaScript comment
+document.write("I have comments in my JavaScript code!");
+//document.write("You can't see this!");
+//-->
+
+document.write("I have multi-line comments!");
+/*document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");
+document.write("You can't see this!");*/
+
+let helloWorld = "Hello World"
+console.log(helloWorld);
+
+const pi = 3.14;
+console.log(pi);
+
+const burjDubai = "828m";
+console.log(burjDubai);
+
+let fullName = "Jan Schmidt"
+console.log(fullName);
+
+const eifelTower = "324m";
+console.log(eifelTower);
+
+const camelCase = "CamelCase";
+console.log(camelCase);
+
+let kursStatus = true;
+console.log(kursStatus);
